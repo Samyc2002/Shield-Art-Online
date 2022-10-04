3709bcbec0808f590b20302ca3bd1503bde631e8
Samyc2002 <2000200402@iitdh.ac.in>
2022-10-05 02:56:51 +0530

    Added login route for users. Need to work on the schemas a bit more.

--- server/src/models/User/UserAuthDetails.ts
+++ server/src/models/User/UserAuthDetails.ts
@@ -1,22 +1,36 @@
 import { Document, model, Model, Schema } from 'mongoose';
+import * as crypto from 'crypto';
 
 interface IUserDetail {
   name: string;
-  avatar: string;
-  work_mail: string;
-  phone: number;
+  // avatar: string;
+  email: string;
+  // phone: number;
+  hash: string;
+  salt: string;
   created_at: string;
   updated_at: string;
+  isActive: boolean;
 }
 export type UserDetailDocument = IUserDetail & Document;
 
-const UserDetailSchema: Schema = new Schema({
+const UserDetailSchema: Schema<UserDetailDocument> = new Schema({
   name: { type: String, required: false, default: '' },
-  avatar: { type: String, required: false },
-  work_mail: { type: String, required: false, default: '' },
-  phone: { type: Number, required: false, default: 0 },
-  created_at: { type: Date, required: false, default: new Date() },
-  updated_at: { type: Date, required: true, default: new Date() },
+  // avatar: { type: String, required: false },
+  email: { type: String, required: false, default: '' },
+  // phone: { type: Number, required: false, default: 0 },
+  hash: String,
+  salt: String,
+  created_at: {
+    type: String,
+    required: false,
+    default: new Date().toISOString()
+  },
+  updated_at: {
+    type: String,
+    required: true,
+    default: new Date().toISOString()
+  },
   isActive: { type: Boolean, required: false, default: false }
 });
 
--- server/src/routers/AuthRouter.ts
+++ server/src/routers/AuthRouter.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { AuthController } from '../controllers/AuthController';
-import { AuthSetup } from '../middlewares/authentication-setup';
-import { AuthValidators } from '../validators/AuthValidators';
+// import { AuthSetup } from '../middlewares/authentication-setup';
+// import { AuthValidators } from '../validators/AuthValidators';
 
 // The file is responsible to process the api requests and call the required middleware, validator and controller in a centralized place
 
@@ -25,8 +25,8 @@ class AuthRouter {
     // add all post routes here
     this.router.post(
       '/login', // path of api request
-      AuthSetup.isAuthenticated, // checks if the request contains a valid token (checks if user is logged in. Remove this middleware if user is supposed to not be logged in).
-      AuthValidators.login, // validates the body content.
+      // AuthSetup.isAuthenticated, // checks if the request contains a valid token (checks if user is logged in. Remove this middleware if user is supposed to not be logged in).
+      // AuthValidators.login, // validates the body content.
       AuthController.login // Main business logic of the server that returns the required response.
     );
   }
