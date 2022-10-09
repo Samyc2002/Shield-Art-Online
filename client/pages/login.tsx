161321b960021bbda3b756aa8cf9a85b046130a8
Tabish Khalid Halim <tkhalim14@gmail.com>
2022-10-09 17:22:13 +0530

    Made Login Page

---
+++ client/pages/login.tsx
@@ -0,0 +1,33 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Login: NextPage = () => {
+  return (
+  <div>
+    <div className={'box'}>
+        <div className={'form'}>
+            <h2>Sign-in</h2>
+            <div className={'inputBox'}>
+                <input type={'text'} required/>
+                <span>Username</span>
+                <i/>
+            </div>
+            <div className={'inputBox'}>
+                <input type={'password'} required/>
+                <span>Password</span>
+                <i/>
+            </div>
+            <div className={'links'}>
+                <a href="#">Forget password</a>
+                <a href="#">Signup</a>
+            </div>
+            <input type={"submit"} value={"Login"}/>
+        </div>
+        
+    </div>
+  </div>
+  )
+}
+
+export default Login
